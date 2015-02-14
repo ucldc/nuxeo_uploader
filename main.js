8af88ab9439f9c251c2eb747cdbedf47b92bc678
BRIAN TINGLE <brian.tingle.cdlib.org@gmail.com>
2015-02-13 23:18:08 -0800

    basic UI

--- main.js
+++ main.js
@@ -1,4 +1,53 @@
 var gui = require('nw.gui');
 var mb = new gui.Menu({type:"menubar"});
-mb.createMacBuiltin("your-app-name");
+mb.createMacBuiltin("nuxeo-uploader");
 gui.Window.get().menu = mb;
+
+var nuxeoConfig = function nuxeoConfig() {
+  function getOrSet(key, text) {
+    var val;
+    if (!localStorage[key]) {
+      localStorage[key] = prompt(text || key);
+    }
+    return localStorage[key];
+  }
+  return { auth: {
+    method: 'proxy',
+    username: getOrSet('username'),
+    token: getOrSet('token')
+  }}
+}
+
+// document ready
+$(function() {
+
+  // set up the page
+  $('#pause').hide();
+  $('#progress, #local, #nuxeo').css('opacity', '0.3');
+
+  $('#select_nuxeo').click(function () {
+    $('#nuxeo').css('opacity', '1');
+    $('#nuxeo .panel-body').html(
+      JSON.stringify(nuxeoConfig())
+    );
+  });
+
+  $('#upload').click(function () {
+    $('#progress').css('opacity', '1');
+    $('#pause').show();
+    $( this ).hide();
+  });
+
+  // http://stackoverflow.com/a/12102992
+  input = $('input');
+  input.click(function () {
+    this.value = null;
+  });
+  input.on('change', function () {
+    $('#local').css('opacity', '1');
+    $('#local .panel-body').html(
+      JSON.stringify(this.files)
+    );
+  });
+  
+});
