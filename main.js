b044981e4aa9663acd6896c1512a2c638e9e8339
BRIAN TINGLE <brian.tingle.cdlib.org@gmail.com>
2015-02-14 14:16:28 -0800

    file selection moved to backbone

--- main.js
+++ main.js
@@ -21,11 +21,71 @@ var nuxeoConfig = function nuxeoConfig() {
 
 // document ready
 $(function() {
+  var File = Backbone.Model.extend({}); 
 
+  var LocalList = Backbone.Collection.extend({
+    model: File
+  });
+
+  var LocalView = Backbone.View.extend({
+    tagName: 'div',
+    initialize: function(){
+      // _.bindAll(this, 'render');
+    },
+    render: function(){
+      var t = _.template('<span title="<%= path %>" class="label label-info"><%= name %></span>');
+      $(this.el).html(t(this.model.attributes));
+      return this;
+    }
+  });
+
+  var ListView = Backbone.View.extend({
+    el: $('#local .panel-body'),
+    events: {
+      'click button#add': 'addItem'
+    },
+    initialize: function(){
+      _.bindAll(this, 'render', 'addFiles', 'appendItem');
+
+      this.collection = new LocalList();
+      this.collection.bind('add', this.appendItem);
+      this.collection.bind('add', function(e) {
+        console.log(e);
+      });
+
+      this.counter = 0;
+      // this.render();
+    },
+    render: function(){
+    },
+    addFiles: function(e){
+      var self = this;
+      _(e).each(function(item) { 
+        self.counter++;
+        var file = new File;
+        file.set(item);
+        self.collection.add(file);
+      });
+    },
+    appendItem: function(item){
+      var itemView = new LocalView({
+        model: item
+      });
+      $(this.el).append(itemView.render().el);
+    }
+  });
+  var listView = new ListView();
+
+  // http://stackoverflow.com/a/12102992
+  input = $('input');
+  input.click(function () {
+    this.value = null;
+  });
+  input.on('change', function () {
+    listView.addFiles(this.files);
+  });
+  
   // set up the page
-  $('#pause').hide();
-  $('#progress, #local, #nuxeo').css('opacity', '0.3');
-
   $('#select_nuxeo').click(function () {
     $('#nuxeo').css('opacity', '1');
 
@@ -43,16 +103,4 @@ $(function() {
     new Notification("Upload Failed!  Not implimented yet");
   });
 
-  // http://stackoverflow.com/a/12102992
-  input = $('input');
-  input.click(function () {
-    this.value = null;
-  });
-  input.on('change', function () {
-    $('#local').css('opacity', '1');
-    $('#local .panel-body').html(
-      JSON.stringify(this.files)
-    );
-  });
-  
 });
