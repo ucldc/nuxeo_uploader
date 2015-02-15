233505a69d96a2a9903c0d92cca2b9cad2a10345
BRIAN TINGLE <brian.tingle.cdlib.org@gmail.com>
2015-02-15 11:26:46 -0800

    refactor

--- nuxeo.js
+++ nuxeo.js
@@ -1,20 +1,30 @@
 'use strict';
 var nuxeo = require('nuxeo');
 var pfa = require("bluebird").promisifyAll;
-pfa(nuxeo);
+nuxeo = pfa(nuxeo);
 
-module.exports = function upload(filename, folder, callback) {
-
-  var client = new nuxeo.Client();
-
+module.exports.nx_status = function nx_status(client, callback){
   client.connectAsync().then(function(){
     console.log("looks like nuxeo is configured and up");
   });
+}
+
+module.exports.upload = function upload(client, params, callback) {
+  /* upload(filename, folder, callback)
+     @filename = local path to a file to upload
+     @folder   = nuxeo document id for Folderish place in nuxeo
+     returns: @callback
+
+   Needs to:
+     - match up file extension to nuxeo object type (?)
+     - checksum file?
+     - upload file, note batch id
+     - create a nuxeo document, attach batch to it
+   */
 
   // find folders the user can write to
   // "Since Nuxeo 6.0-HF06 or Nuxeo 7.2 you can use ecm:acl"
   // http://doc.nuxeo.com/display/NXDOC/NXQL
-
   var nxql = 'select * from Document' +
              //' WHERE ecm:acl/*1/permission' +
              //' IN ("ReadWrite")';
@@ -31,7 +41,7 @@ module.exports = function upload(filename, folder, callback) {
     console.log(data[0].errorMessage, data[0].entries);
   });
 
-  var new_folder = pfa(client.operation('Document.Create')
+  pfa(client.operation('Document.Create')
     .params({
       type: 'Folder',
       name: 'My Folder',
@@ -39,15 +49,16 @@ module.exports = function upload(filename, folder, callback) {
     .input('doc:/')
   ).executeAsync().then(function(data){
       console.log(data[0]);
-    }
-  );
+  });
 
-  console.log(new_folder);
-  console.log(new_folder.data);
-
-  return callback(null, client);
+  return callback(null);
 };
 
 if(require.main === module) {
-  module.exports(process.argv[2], process.argv[3], function(){});
+  var client = new nuxeo.Client();
+  module.exports.nx_status(client);
+  /* module.exports.upload(client,
+                        { file: process.argv[2],
+                        folder: process.argv[3] },
+                        function(){}); */
 } 
