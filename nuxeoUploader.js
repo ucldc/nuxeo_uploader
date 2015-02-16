c67e0b75020bf81292023bcab6a37f0d0a276ecd
BRIAN TINGLE <brian.tingle.cdlib.org@gmail.com>
2015-02-15 17:15:34 -0800

    uploads again

--- nuxeoUploader.js
+++ nuxeoUploader.js
@@ -7,26 +7,18 @@ var pfa = require("bluebird").promisifyAll;
 var path = require('path');
 nuxeo = pfa(nuxeo);
 
-module.exports.type = function type(file, patterns){
-  var ext = path.extname(file);
-  switch(true) {
-    case /jpe?g|tif?|gif|png|svg/i.test(file):
-      return 'Picture';
-    case /mov|mp4/i.test(file):
-      return 'Video';
-    case /wav|mp3/i.test(file):
-      return 'Audio';
-    default:
-      return 'File';
-  }
-}
-
+/*
+ * get nuxeo status
+ */
 module.exports.nx_status = function nx_status(client, callback){
   client.connectAsync().then(function(){
     console.log("looks like nuxeo is configured and up");
   });
 }
 
+/*
+ * get writable locations
+ */
 module.exports.writable = function writable(client, callback){
   // find folders the user can write to
   // "Since Nuxeo 6.0-HF06 or Nuxeo 7.2 you can use ecm:acl"
@@ -47,32 +39,38 @@ module.exports.writable = function writable(client, callback){
   });
 }
 
+/*
+ * upload a file to Nuxeo
+ */
 module.exports.upload = function upload(client, params, callback) {
   var stats = fs.statSync(params.file);
   var file = rest.file(params.file, null, stats.size, null, null);
 
   var uploader = client.operation('FileManager.Import')
     .context({ currentDocument: params.folder })
-    .uploader.uploadFile();
-
-  console.log('x1 about to exec');
+    .input(file)
+    .uploader();
+    
+  uploader.uploadFile(file);
   uploader.execute(function(error, data) {
-    console.log('x2 executed');
     if (error) {
-      throw error;
+      return error;
     }
-    console.log(data);
-    console.log('x2--');
+    if (!data.entries.length) {
+      console.log('hey;');
+      return new Error('Upload/execute returned w/o errors, but `entries` is empty');
+    }
+    console.log(data.entries[0].uid, data.entries[0].path);
   });
 }
 
-
-
+/*
+ * if this is running as a script
+ */
 if(require.main === module) {
   var client = new nuxeo.Client();
-  console.log(module.exports.type(process.argv[2]));
-  module.exports.upload(client,
+  var status = module.exports.upload(client,
                         { file: process.argv[2],
                           folder: '/default-domain/workspaces/test' },
-                        function(){}); 
+                        function(s){console.log(s);}); 
 } 
