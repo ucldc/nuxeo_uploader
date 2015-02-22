aad06b6146ddcad57032ad816f54ca0e0a23373e
BRIAN TINGLE <brian.tingle.cdlib.org@gmail.com>
2015-02-22 00:14:38 -0800

    working with grunt now (app or dist)

--- Gruntfile.js
+++ Gruntfile.js
@@ -323,7 +323,8 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             'images/{,*/}*.webp',
             '{,*/}*.html',
-            'styles/fonts/{,*/}*.*'
+            'styles/fonts/{,*/}*.*',
+            '*.js'
           ]
         }, {
           src: 'node_modules/apache-server-configs/dist/.htaccess',
