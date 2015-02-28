fbaa72e644642d640849033bafc6ccdf23872b29
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2015-02-27 17:23:30 -0800

    starting re-factor/re-design

--- main.js
+++ main.js
@@ -23,23 +23,6 @@ var NuxeoUploadApp = new Backbone.Marionette.Application();
 NuxeoUploadApp.on("start", function(options){
   logger.info('application starting');
 
-
-  /*
-   *  nuxeo config
-           .nuxeo-config #nuxeo_server #nuxeo_token #auth_token_link
-   */
-
-  // poor man's data binding
-  $('#auth_token_link').on('click', function(event, baseURL) {
-    // open a window that is big enough for shibboleth
-    var new_win = gui.Window.open(
-      url.resolve(
-        $('#nuxeo_server').val(),
-        path.join('nuxeo', nuxeoupload.get_auth_token_link())
-      )
-    );
-  });
-
   // model for configuration object
   var ConfigModel = Backbone.Model.extend({
     localStorage: new Backbone.LocalStorage("nuxeo_uploader_config"),
@@ -115,138 +98,47 @@ NuxeoUploadApp.on("start", function(options){
    *  set up Models and Views for file processing
    */
 
-  // model for a file
-  var File = Backbone.Model.extend({});
-
-  // Files selected by the user for upload
-  //  items will get pull off list as uploaded
-  var LocalList = Backbone.Collection.extend({ model: File });
-  var UploadingList = Backbone.Collection.extend({ model: File });
-  var FinishedList = Backbone.Collection.extend({ model: File });
-
-  // View a file (independent of the list/state that it is in)
-  var FileView = Backbone.View.extend({
-    tagName: 'div',
-    initialize: function(){
-      // _.bindAll(this, 'render');
-    },
-    render: function(){
-      var t = _.template('<span title="<%= path %>" class="label label-info"><%= name %></span>');
-      $(this.el).html(t(this.model.attributes));
-      return this;
+  var File = Backbone.Model.extend({
+    defaults: {
+      state: 'selected'
     }
   });
-
-  // View for local files
-  var LocalListView = Backbone.View.extend({
-    el: $('#local .panel-body'),
+  var FileCollection = Backbone.Collection.extend({ model: File });
+  var FileView = Backbone.Epoxy.View.extend({});
+  var FileListView = Backbone.Epoxy.View.extend({
+    el: '#local',
+    itemView: FileView,
     initialize: function(){
-      _.bindAll(this, 'render', 'addFiles', 'appendItem', 'removeItem');
-
-      this.collection = new LocalList();
-      this.collection.bind('add', this.appendItem);
-      this.collection.bind('remove', this.removeItem);
-
-      this.counter = 0;
-      // this.render();
+      this.collection = new FileCollection();
     },
-    // add an array of files
-    addFiles: function(e){
-      var self = this;
-      _(e).each(function(item) {
-        self.counter++;
+    addFiles: function(e) {
+      var that = this;
+      _(e).each(function(item){
+        console.log(e);
+        that.counter++;
         var file = new File;
         file.set(item);
-        self.collection.add(file);
+        that.collection.add(file);
       });
-    },
-    appendItem: function(file){
-      var fileView = new FileView({
-        model: file
-      });
-      $(this.el).append(fileView.render().el);
-    },
-    removeItem: function(file){
-      $(this.el).remove();
     }
   });
+  var fileListView = new FileListView();
 
-  // View for finished files
-  var FinishedListView = Backbone.View.extend({
-    el: $('#nuxeo .panel-body'),
-    initialize: function(){
-      _.bindAll(this, 'render', 'appendItem');
+  /*
+   *  nuxeo config
+           .nuxeo-config #nuxeo_server #nuxeo_token #auth_token_link
+   */
 
-      this.collection = new FinishedList();
-      this.collection.bind('add', this.appendItem);
-      this.collection.bind('remove', this.removeItem);
-
-      this.counter = 0;
-      // this.render();
-    },
-    appendItem: function(file){
-      var fileView = new FileView({
-        model: file
-      });
-      $(this.el).append(fileView.render().el);
-    },
+  // poor man's data binding
+  $('#auth_token_link').on('click', function(event, baseURL) {
+    // open a window that is big enough for shibboleth
+    var new_win = gui.Window.open(
+      url.resolve(
+        $('#nuxeo_server').val(),
+        path.join('nuxeo', nuxeoupload.get_auth_token_link())
+      )
+    );
   });
-
-  var finishedListView = new FinishedListView();
-
-  // View for in progress files
-  var UploadingListView = Backbone.View.extend({
-    el: $('#progress .panel-body'),
-    initialize: function(){
-      _.bindAll(this, 'render', 'addFiles', 'appendItem', 'removeItem');
-
-      this.collection = new UploadingList();
-      this.collection.bind('add', this.appendItem);
-      this.collection.bind('remove', this.removeItem);
-
-      this.counter = 0;
-      // this.render();
-    },
-    // add an array of files
-    addFiles: function(e){
-      var self = this;
-      console.log(e);
-      _(e).each(function(item) {
-        self.counter++;
-        var file = new File;
-        file.set(item);
-        self.collection.add(file);
-      });
-    },
-    appendItem: function(file){
-      var me = this;
-      var fileView = new FileView({
-        model: file
-      });
-      // upload when it is added to this collection
-      nuxeoupload.upload(
-        client,
-        { file: file,
-          folder: '/asset-library/' + $('select').val()
-        },
-        // callback for when its uploaded, move it to the next collection
-        function(bb_file, file, data){
-          console.log('upload callback');
-          console.log(bb_file, data);
-          me.collection.remove(bb_file);
-          finishedListView.collection.add(bb_file);
-        }
-      );
-      $(this.el).append(fileView.render().el);
-    },
-    removeItem: function(file){
-      $(this.el).remove();
-    }
-  });
-  var uploadingListView = new UploadingListView();
-
-  var localListView = new LocalListView();
-
 
   /*
    *  Select files for upload
@@ -259,7 +151,7 @@ NuxeoUploadApp.on("start", function(options){
     this.value = null;
   });
   input.on('change', function () {
-    localListView.addFiles(this.files);
+    fileListView.addFiles(this.files);
     this.disabled = true;
     $(this).addClass('btn-default').removeClass('btn-primary');
     $('#upload').addClass('btn-primary');
@@ -290,11 +182,10 @@ NuxeoUploadApp.on("start", function(options){
    */
   $('#upload').click(function () {
     var $btn = $(this).button('uploading files to Nuxeo');
-    while(localListView.collection.length) {
+    /* while(fileListView.collection.length) {
       var file = localListView.collection.pop();
       logger.debug(file.attributes);
-      uploadingListView.collection.add(file.attributes);
-    }
+    } */
     // $btn.button('reset')
     // new Notification("Upload Failed!  Not implimented yet");
   });
